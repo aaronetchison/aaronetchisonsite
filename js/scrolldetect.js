6416f46e82d603b51f777032dd5fcb013e7c42af
Aaron Etchison <abetchison@gmail.com>
2015-02-20 21:55:49 -0500

    scroll detect files created

---
+++ js/scrolldetect.js
@@ -0,0 +1,68 @@
+
+
+// Config
+// =================================================
+
+var $root          = $('html'),
+    $nav_header    = $('.banner'),
+    $navicon       = $('[data-navicon="button"]'),
+    header_height  = $('.banner').height(),
+    hero_height    = $('.hero').height(),
+    offset_val     = hero_height - header_height,
+    clickEvent     = ((document.ontouchstart !== null) ? 'click' : 'touchstart');
+
+
+// Methods
+// =================================================
+
+function navSlide() {
+  var scroll_top = $(window).scrollTop();
+
+  if (scroll_top >= offset_val) {
+    $nav_header.addClass('is-sticky');
+  } else {
+    $nav_header.removeClass('is-sticky');
+  }
+}
+
+function menuToggle() {
+  if($nav_header.hasClass('is-open')) {
+    $root.removeClass('pinned');
+    $nav_header.removeClass('is-open');
+    $navicon.removeClass('is--closed');
+  } else {
+    $root.addClass('pinned');
+    $nav_header.addClass('is-open');
+    $navicon.addClass('is--closed')
+  }
+}
+
+function openNav() {
+  if ($nav_header.hasClass('is-open')) {
+    $nav_header.removeClass('is-open');
+    $root.removeClass('pinned');
+    $navicon.removeClass('is--closed');
+  }
+}
+
+function anchorScroll(event) {
+  var id     = $(this).attr("href"),
+      offset = header_height,
+      target = $(id).offset().top - offset;
+
+  $('html, body').animate({
+    scrollTop: target
+  }, 500);
+
+  event.preventDefault();
+}
+
+
+// Handlers
+// =================================================
+
+$('.scrollto').on(clickEvent, function() { anchorScroll.call(this, event); });
+$navicon.on(clickEvent, menuToggle);
+$('.banner a').on(clickEvent, openNav);
+$(window).scroll(navSlide);
+
